adf18db70f876a5e06270e697ed1de3ac23253dc
Ana Jamyle <goncalvesanajamile@gmail.com>
2026-06-14 21:58:16 -0300

    feat: estrutura inicial do projeto

---
+++ cardapio.js
@@ -0,0 +1,21 @@
+const cardapio = [
+    {
+        nome: "X-Burguer",
+        preco: 18.90,
+        categoria: "Lanche"
+    },
+    {
+        nome: "X-Bacon",
+        preco: 21.90,
+        categoria: "Lanche"
+    },
+    {
+        nome: "Combo Família",
+        preco: 45.90,
+        categoria: "Combo"
+    }
+];
+
+console.log("Cardápio carregado:");
+console.log(cardapio);
+console.log("Sistema do cardápio iniciado");
